3547ec0e59cfa1d3bfe4d3fa9bee4c0108f9dc21
TayKieu <kieuduongtay@gmail.com>
2023-03-11 18:06:50 +0700

    nop bai

--- module3/baitap1.sql
+++ module3/baitap1.sql
@@ -1,1 +1,22 @@
-SELECT * FROM student.teacher;
+use student;
+create table class(
+id_class int,
+name_class varchar(45),
+primary key(id_class));
+
+create table teacher(
+id_teacher int,
+name_teacher varchar(45),
+age_teacher int,
+country_teacher varchar(45),
+primary key (id_teacher));
+
+select* from class;
+insert into class(id_class,name_class)
+value(1,'a0922i1');
+
+select* from teacher;
+insert into teacher(id_teacher,name_teacher,age_teacher,country_teacher)
+value(1,'Quoc',30,'danang');
+
+
