8630a7901228d87fd9c27d915f44234eb7f5bbf8
TayKieu <kieuduongtay@gmail.com>
2023-09-27 00:41:06 +0700

    25/09/2023

---
+++ md5/bai_tap/reactform_router/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import {Validate_ContactForm} from "./component/Validate_ContactForm";
+
+function App() {
+  return (
+    <Validate_ContactForm></Validate_ContactForm>
+  );
+}
+
+export default App;
