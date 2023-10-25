ab10fb31b377445998f431fa6d341b041d26945b
TayKieu <kieuduongtay@gmail.com>
2023-10-25 17:08:10 +0700

    ok

--- md5/bai_tap/product/src/component/List.js
+++ md5/bai_tap/product/src/component/List.js
@@ -1,19 +1,24 @@
 import * as product_service from "../service/product_service"
+import * as category_service from "../service/category_service"
 import {useEffect, useState} from "react";
 import {Paginate} from "./Paginate";
 export function List() {
     const [products, setProducts] = useState([])
+    const [categories, setCategories] = useState([])
 
     useEffect(() => {
             const fetchApi = async () => {
-                const result = await product_service.findAll()
-                setProducts(result)
+                const result1 = await product_service.findAll()
+                setProducts(result1)
+                const result2 = await category_service.findAllCategories()
+                setCategories(result2)
             }
             fetchApi()
     }, []);
     return (
         <>
             <Paginate data={products}></Paginate>
+            {/*<Paginate data={categories}></Paginate>*/}
         </>
     )
 }
--- md5/bai_tap/product/src/component/Paginate.js
+++ md5/bai_tap/product/src/component/Paginate.js
@@ -3,29 +3,49 @@ import ReactPaginate from "react-paginate";
 import {toast, ToastContainer} from "react-toastify";
 import {NavLink} from "react-router-dom";
 import * as product_service from "../service/product_service";
+import * as category_service from "../service/category_service";
 
-export function Paginate(props){
+export function Paginate(props) {
     const [key, setKey] = useState('')
     const {data} = props
     console.log(data)
-    const [currentItems,setCurrentItems] =useState([])
-    const [pageCount,setPageCount]= useState(0)
-    const [itemOffset, setItemOffset] = useState(0)
-    const itemsPerPage = 2
+    const [currentItems, setCurrentItems] = useState([])
+    // const [pageCount, setPageCount] = useState(0)
+    // const [itemOffset, setItemOffset] = useState(0)
 
-    useEffect(()=>{
-        const endOffset = itemOffset + itemsPerPage
-        if(key ==="") {
-            setCurrentItems(data.slice(itemOffset, endOffset))
+
+
+    const [currentPage,setCurrentPage]=useState(1)
+    const recordsPerPage = 3
+    const lastIndex = currentPage * recordsPerPage
+    const firstIndex = lastIndex - recordsPerPage
+    const records = data.slice(firstIndex,lastIndex)
+    const npage = Math.ceil(data.length/recordsPerPage)
+    const numbers = [...Array(npage + 1).keys()].slice(1)
+
+
+    // const itemsPerPage = 2
+    //Category
+    const [categories, setCategories] = useState([])
+    useEffect(() => {
+        // const endOffset = itemOffset + itemsPerPage
+        // if (key === "") {
+        //     setCurrentItems(records)
+        // }
+        // setPageCount(Math.ceil(data.length / itemsPerPage))
+
+        const fetchApi = async () => {
+            const result = await category_service.findAllCategories()
+            setCategories(result)
         }
-        setPageCount(Math.ceil(data.length / itemsPerPage))
+        fetchApi()
+    },[data,key])
+    // }, [itemOffset, itemsPerPage, data, key])
 
-    },[itemOffset,itemsPerPage,data,key])
-
-    const handlePageClick = (event)=>{
-        const newOffset = (event.selected + itemsPerPage) % data.length
-        setItemOffset(newOffset)
-    }
+    // const handlePageClick = (event) => {
+    //     const newOffset = (event.selected + itemsPerPage) % data.length
+    //     setItemOffset(newOffset)
+    // }
     const handleDelete = async (id) => {
         await product_service.deleteById(id)
         setCurrentItems((prevProduct) => prevProduct.filter((product) => product.id !== id));
@@ -44,71 +64,105 @@ export function Paginate(props){
         }
         fetchApi()
     }
-    return(
+    const prePage = () =>{
+        if(currentPage !== firstIndex){
+            setCurrentPage(currentPage-1)
+        }
+    }
+    const changePage = (id) =>{
+        setCurrentPage(id)
+    }
+    const nextPage = () =>{
+        if(currentPage !== lastIndex){
+            setCurrentPage(currentPage+1)
+        }
+    }
+    return (
         <>
             <div>
                 <h1>Product list</h1>
-            <div>
                 <div>
-                    <input type="search"
-                           id="form1"
-                           value={key}
-                           onChange={changeSearch}
-                    />
-                    <button className="btn btn-success" onClick={handleSearch}>Find</button>
+                    <div>
+                        <input type="search"
+                               id="form1"
+                               value={key}
+                               onChange={changeSearch}
+                        />
+                        <button className="btn btn-success" onClick={handleSearch}>Find</button>
+                    </div>
+
                 </div>
-
-            </div>
-            <div>
-                <ToastContainer/>
-            </div>
-            <NavLink to='/create' className='btn btn-success'>Add new product</NavLink>
-            <table className='table'>
-                <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Quantity</th>
-                    <th>Description</th>
-                    <th>Price</th>
-                    <th>Action</th>
-                </tr>
-                </thead>
-                <tbody>
-                {currentItems.map((p) => (
+                <div>
+                    <ToastContainer/>
+                </div>
+                <NavLink to='/create' className='btn btn-success'>Add new product</NavLink>
+                <table className='table'>
+                    <thead>
                     <tr>
-                        <td>{p.name}</td>
-                        <td>{p.quantity}</td>
-                        <td>{p.description}</td>
-                        <td>{p.price}</td>
-                        <td>
-                            <NavLink to={`/update/${p.id}`} className='btn btn-primary'>Edit</NavLink>
-                            <button className="btn btn-danger"
-                                    onClick={(e) => handleDelete(p.id)}>Delete
-                            </button>
-
-                        </td>
+                        <th>Name</th>
+                        <th>Quantity</th>
+                        <th>Description</th>
+                        <th>Price</th>
+                        <th>Category</th>
+                        <th>Action</th>
                     </tr>
-                ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                    {records.map((p) => (
+                        <tr>
+                            <td>{p.name}</td>
+                            <td>{p.quantity}</td>
+                            <td>{p.description}</td>
+                            <td>{p.price}</td>
+                            <td>{
+                                categories.find(category => String(category.id) === String(p.category))?.name
+                            }</td>
+                            <td>
+                                <NavLink to={`/update/${p.id}`} className='btn btn-primary'>Edit</NavLink>
+                                <button className="btn btn-danger"
+                                        onClick={(e) => handleDelete(p.id)}>Delete
+                                </button>
+
+                            </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
             </div>
-            <ReactPaginate
-                breakLabel="..."
-                nextLabel="next >"
-                onPageChange={handlePageClick}
-                pageRangeDisplayed={2}
-                pageCount={pageCount}
-                previousLabel="< previous"
-                pageClassName="page-item"
-                pageLinkClassName="page-link"
-                previousClassName="page-item"
-                previousLinkClassName="page-link"
-                nextClassName="page-item"
-                nextLinkClassName="page-link"
-                breakClassName="page-item"
-                breakLinkClassName="page-link"
-                containerClassName="pagination"
-                activeClassName="active"
-            /></>
+            {/*<ReactPaginate*/}
+            {/*    breakLabel="..."*/}
+            {/*    nextLabel="next >"*/}
+            {/*    onPageChange={handlePageClick}*/}
+            {/*    pageRangeDisplayed={2}*/}
+            {/*    pageCount={pageCount}*/}
+            {/*    previousLabel="< previous"*/}
+            {/*    pageClassName="page-item"*/}
+            {/*    pageLinkClassName="page-link"*/}
+            {/*    previousClassName="page-item"*/}
+            {/*    previousLinkClassName="page-link"*/}
+            {/*    nextClassName="page-item"*/}
+            {/*    nextLinkClassName="page-link"*/}
+            {/*    breakClassName="page-item"*/}
+            {/*    breakLinkClassName="page-link"*/}
+            {/*    containerClassName="pagination"*/}
+            {/*    activeClassName="active"*/}
+            {/*/>*/}
+            <nav>
+                <ul className='pagination'>
+                    <li className='page-item'>
+                        <a href='#' className='page-link' onClick={prePage}>Prev</a>
+                    </li>
+                    {numbers.map((n,i)=>(
+                        <li className={`page-item ${currentPage === n ? 'active':''}`} key={i}>
+                            <a href='#' className='page-link'
+                            onClick={()=>changePage(n)}>{n}</a>
+                        </li>
+                    ))}
+                    <li className='page-item'>
+                        <a href='#' className='page-link' onClick={nextPage}>Next</a>
+                    </li>
+                </ul>
+            </nav></>
+
     )
 }
